c3937a25566de0b1f17012cc520523e287d96706
misterjtc <jordan@jordancook.dev>
2021-12-19 19:56:54 -0500

    finished all of the site content and styling. Finished the responsive design.

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,7 @@
 // Main namespace object
 const meekMittApp = {};
 
+// Hero slider function
 meekMittApp.hero = function() {
     $('.heroMain').flickity({
         // options
@@ -11,10 +12,61 @@ meekMittApp.hero = function() {
         prevNextButtons: false
     });
 };
+
+// Smoothscroll function
+meekMittApp.smoothScroll = function () {
+    // Smooth scroll to home
+    $('.homeButton').on('click', function() {
+        $.smoothScroll({
+          scrollTarget: '#home',
+          speed: 800
+        });
+        return false;
+      });
+    // Smooth scroll to menu section
+    $('.menuButton').on('click', function() {
+        $.smoothScroll({
+          scrollTarget: '#menu',
+          speed: 800
+        });
+        return false;
+      });
+};
+// Mobile nav menu actuation
+meekMittApp.navMenu = function() {
+    // On click of the mobile nav icon expand or collapse the nav
+    $(".headerMobileNavLink").on("click", function(){
+         $navHeight = $('header').height();
+        console.log($navHeight);
+        // If the header bar has class navCheck collapse it - else expand it
+        if ( $('header').hasClass("navCheck") ) {
+        // collapse nav
+          $('header').animate({height: "50px"}, 400, "linear");
+          $('header').removeClass('navCheck');
+          $(".mobileList").fadeOut( "fast", function() {
+            // Animation complete
+          });
+        } else {
+          // expand nav
+          $('header').animate({height: "250px"}, 400, "linear");
+          $('header').addClass('navCheck');
+          $('.mobileList').css("display", "flex");
+          $(".mobileList").fadeIn( "fast", function() {
+            // Animation complete
+          });
+        }
+      });
+}
+
+
+// Init function
 meekMittApp.init = function() {
     // fire up the hero
     meekMittApp.hero();
+    meekMittApp.smoothScroll();
+    meekMittApp.navMenu();
 };
+
 // Document ready function
 $(document).ready(function() {
     console.log("I'm Ready");
